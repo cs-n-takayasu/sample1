32c959e41e625b5f6121cf40ac4a6ca16d14de6d
cs-n-takayasu <n.takayasu@cancerscan.jp>
2023-04-13 14:07:14 +0900

    add test

---
+++ tests/test_addDef.py
@@ -0,0 +1,13 @@
+import pytest
+from src.addDef import addDef
+
+
+@pytest.mark.parametrize(
+    "num1, num2, result",
+    [(0, 1, 1)],
+)
+def test_addDef(num1, num2, result):
+    """
+    関数をパラメータ化して、addDefを検証
+    """
+    assert result == addDef(num1, num2)
