a1b9c7e0c986cd58d1e899a723ce56624087ab0f
cs-n-takayasu <n.takayasu@cancerscan.jp>
2023-04-13 16:15:14 +0900

    split wf

---
+++ src/banditSample.py
@@ -0,0 +1,15 @@
+
+import yaml
+
+
+class Sample():
+    def __init__(self, password: str, data_path: str):
+        self.password = password
+        assert isinstane(password, int)
+
+        if password == "root":
+            self.is_admin = True
+        else:
+            self.is_admin = False
+        
+        self.data = yaml.load(data_path)
