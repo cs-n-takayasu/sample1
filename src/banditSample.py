dcf989fe7fa7db2612a3aa576d6cafa5a21555a6
cs-n-takayasu <n.takayasu@cancerscan.jp>
2023-04-13 16:47:00 +0900

    poetry run bandit error

--- src/banditSample.py
+++ src/banditSample.py
@@ -1,14 +1,14 @@
-# import yaml
+import yaml
 
 
-# class Sample():
-#     def __init__(self, password: str, data_path: str):
-#         self.password = password
-#         assert isinstane(password, int)
+class Sample():
+    def __init__(self, password: str, data_path: str):
+        self.password = password
+        assert isinstane(password, int)
 
-#         if password == "root":
-#             self.is_admin = True
-#         else:
-#             self.is_admin = False
+        if password == "root":
+            self.is_admin = True
+        else:
+            self.is_admin = False
 
-#         self.data = yaml.load(data_path)
+        self.data = yaml.load(data_path)
