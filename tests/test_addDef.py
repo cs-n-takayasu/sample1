66754346718bf9a66f4757994fb610fc9aff3062
cs-n-takayasu <n.takayasu@cancerscan.jp>
2023-04-14 11:09:52 +0900

    test error

--- tests/test_addDef.py
+++ tests/test_addDef.py
@@ -10,4 +10,5 @@ def test_addDef(num1, num2, result):
     """
     関数をパラメータ化して、addDefを検証
     """
-    assert result == addDef(num1, num2)
+    # assert result == addDef(num1, num2)
+    assert 2 == addDef(num1, num2)
